aaeb0c3284c17dcce5393be9db977837640f03e6
guthhm <guthhm@gmail.com>
2019-08-20 06:26:36 +0100

    Created routes to adress devs informations into MongoDB

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,23 @@
-const express = require('express')
+const express = require('express') // Importa o express
+const DevController = require('./controller/dev_controller')
+const LikeController = require('./controller/like_controller')
+const DislikeController = require('./controller/dislike_controller')
 
-const routes = express.Router()
+const routes = express.Router() // Função que cria um objeto específico para rotas
 
-routes.get('/', (req, res) => { // Obter da rota padrao "/" uma requisicao e dar uma resposta
+/*routes.get('/', (req, res) => { // Obter da rota padrao "/" uma requisicao e dar uma resposta
     return res.json({message: `Hello ${req.query.name}`}); // query busca os parametros passados na URL
-    /* Nao e interessante enviar somente uma mensagem com "send" pelo servidor, geralmente envia-se um objeto json com uma chave */
+    /* Nao e interessante enviar somente uma mensagem com "send" pelo servidor, geralmente envia-se um objeto json com uma chave 
 });
 
-module.exports = routes
+routes.post('/devs', (req, res) => {
+    return res.json(req.body)  // Recebe como JSON o corpo da requisicao
+})*/
+
+routes.get('/devs', DevController.index)
+
+routes.post('/devs', DevController.store)
+routes.post('/devs/:devID/likes', LikeController.store)
+routes.post('/devs/:devID/dislikes', DislikeController.store)
+
+module.exports = routes // Expõe a variável routes para o servidor
